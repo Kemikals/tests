d660fef1a5646e8e58e5dcf75b4b30c14333f167
Kemikals <actahers@hawaii.edu>
2021-03-27 13:02:24 -1000

    more revisions

--- src/console/BufferedReadWriteVersion.java
+++ src/console/BufferedReadWriteVersion.java
@@ -1,58 +1,68 @@
 package console;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class BufferedReadWriteVersion {
 
+    private static final String COUNTRIES_FILE = "resources/countries.txt";
+
     public static void main(String[] args) {
+
+        List<String> countries = getCountriesFromFile(COUNTRIES_FILE);
+
+        System.out.println("Welcome To Country name Guessing Game");
+
         Scanner input = new Scanner(System.in);
         int score = 0;
-        int wordCounter = 0;
-        String tryAgain;
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader("src/console/countries.txt"));
-            ArrayList<String> originalOrderList = new ArrayList<>();
+
+        do {
+            String randomCountry = countries.get(ThreadLocalRandom.current().nextInt(countries.size()));
+            printQuestion(randomCountry);
+            String guess = input.nextLine();
+
+            if (guess.equalsIgnoreCase(randomCountry)) {
+                System.out.println("Good Job That is Correct !");
+                score++;
+            } else {
+                System.out.println("Wrong Guess , The country name was : [" + randomCountry.toUpperCase() + "]");
+            }
+
+            System.out.println("Play Again? y for yes -or- n or just enter for no");
+
+        } while (input.nextLine().equalsIgnoreCase("y"));
+
+        System.out.println("YOUR SCORE IS " + score);
+    }
+
+    private static List<String> getCountriesFromFile(String path) {
+        List<String> originalOrderList = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+
             String line;
             while ((line = reader.readLine()) != null) {
                 originalOrderList.add(line);
             }
-            Collections.shuffle(originalOrderList);
-            System.out.println("Welcome To Country name Guessing Game");
-            do {
-
-                char c1 = originalOrderList.get(wordCounter).charAt(0);
-                char c2 = originalOrderList.get(wordCounter).charAt(1);
-
-                System.out.println("What is the name of the country that \nstart with [" + c1 + "]"
-                        + "And 2nd with with [" + c2 + "] ?"
-                        + "\n*HINT:this Country name has a: [" + originalOrderList.get(wordCounter).length() + "] Letters");
-                System.out.println("THE COUNTRY NAME IS :->");
-                String guess = input.nextLine();
-                if (guess.equalsIgnoreCase(originalOrderList.get(wordCounter))) {
-                    System.out.println("Good Job That is Correct !");
-                    score++;
-
-                } else {
-                    System.out.println("Wrong Guess , The country name was : [" + originalOrderList.get(wordCounter).toUpperCase() + "]");
-                }
-                wordCounter++;
-                System.out.println("Play Again? y for yes -or- n or just enter for no");
-                tryAgain = input.nextLine();
-            } while (tryAgain.equalsIgnoreCase("y"));
-            System.out.println("YOUR SCORE IS " + score);
-
-
         } catch (IOException e) {
-            System.out.println("FILE NOT FOUND ");
-        } catch (Exception z) {
-            input.close();
+            System.err.println("Unable to read countries file");
+            System.exit(-1);
         }
 
+        Collections.shuffle(originalOrderList);
+
+        return originalOrderList;
     }
 
+    private static void printQuestion(String country) {
+        System.out.println("What is the name of the country that \nstarts with " + country.substring(0, 2));
+        System.out.println("*HINT:this Country name has a: [" + country.length() + "] Letters\n");
+        System.out.print("THE COUNTRY NAME IS : ");
+    }
 }
